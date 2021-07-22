163e76ef0b1bd90972d483aea1ea9683fa6ced27
AKuznetsov <alex.kuznetsov@osi.ca.gov>
2021-07-22 15:39:37 -0700

    Run psql scripts in background

--- backup.sh
+++ backup.sh
@@ -81,7 +81,7 @@ echo "Dump of row_item_part1_$(date +"%Y-%m-%d").json done"
 psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE -w -c "COPY (WITH t2 AS (select row_to_json(t) as js from cans_report.assessment_item_v t where mod(assessment_id,2) = 0), t1 AS (SELECT ARRAY_TO_JSON(array_agg(js)) as "assessment_item_v" FROM t2) SELECT row_to_json(t1) FROM t1) TO STDOUT WITH ENCODING 'UTF-8';" | sed 's/\\\\/\\/g' | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/row_item_part2_$(date +"%Y-%m-%d").json || exit 2
 echo "Dump of row_item_part2_$(date +"%Y-%m-%d").json done"
 
-echo "$(aws $AWS_ARGS s3 cp s3://$S3_BUCKET/$S3_PREFIX/row_item_part2_$(date +"%Y-%m-%d").json -) $(aws $AWS_ARGS s3 cp s3://$S3_BUCKET/$S3_PREFIX/row_item_part1_$(date +"%Y-%m-%d").json -)" | /usr/bin/jq -s '.[0].assessment_item_v + .[1].assessment_item_v | {assessment_item_v: .}' | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/row_item_$(date +"%Y-%m-%d").json || exit 2
+echo "$(aws $AWS_ARGS s3 cp s3://$S3_BUCKET/$S3_PREFIX/row_item_part2_$(date +"%Y-%m-%d").json -) $(aws $AWS_ARGS s3 cp s3://$S3_BUCKET/$S3_PREFIX/row_item_part1_$(date +"%Y-%m-%d").json -)" | /usr/bin/jq -c -s '.[0].assessment_item_v + .[1].assessment_item_v | {assessment_item_v: .}' | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/row_item_$(date +"%Y-%m-%d").json || exit 2
 echo "Dump of row_item_$(date +"%Y-%m-%d").json done"
 
 psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE -w -c "COPY (WITH t2 AS (select row_to_json(t) as js from cans_report.assessment_v t), t1 AS (SELECT ARRAY_TO_JSON(array_agg(js)) as \"assessment_v\" FROM t2) SELECT row_to_json(t1) FROM t1) TO STDOUT WITH ENCODING 'UTF-8'" | sed 's/\\\\/\\/g' | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/row_assessment_$(date +"%Y-%m-%d").json || exit 2
